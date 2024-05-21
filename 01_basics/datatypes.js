eac3da3c1f3151f80974ee5941eaa9379b204bc6
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-05-21 16:47:44 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer versions
+
+// alert("new") we are using nodejs not browser
+
+
+let name = 'preksha' //string
+let age =24 //integer
+let isLoggedIn = false //boolean
+
+//types of datatypes
+//number => 2 to power 53
+//null => stand alone value, intentionally making any variable empty
+//undefined => value is declared but not defined.
+// symbol => to find uniqueness
+// object
+
+console.log(typeof undefined) //output is undefined
+console.log(typeof null) //output is object
