115ca8ea59485041693dd534776152ded7e5f3dc
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-05-23 16:06:10 +0000

    stack and heap

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -15,3 +15,29 @@ const heros = ["Spiderman","superman","batman"]
   let myFunction =function(){
     console.log("hello world");
   }
+
+
+  /////////////////////////////////////
+
+  // stack(primitive) and heap memory(non-primitive)
+  //whenever stack memory -> whenever a variable is declared we will get a copy, whatever changes we do copy value gets changed
+  //whenever heap memroy -> we get reference/ original value , whatever changes we do original value gets changed
+//stack mempory , when change is made it is made to the copy value not the original so original value is not changed.
+  let myYt = "preksha"
+  let anotherName= myYt
+  anotherName = "Rajath"
+  console.log(myYt)
+  console.log(anotherName)
+
+  //heap memory, it takes reference so when userTwo's email got changed userOnes also got changed because it was already declared equal
+  // all the changes are done to original
+  let userOne = {
+    email:"preksha@google.com",
+    id:23
+  }
+  let userTwo = userOne
+  userTwo.email = "gunda@google.com"
+
+  console.log(userOne.email);
+  console.log(userTwo.email);
+
