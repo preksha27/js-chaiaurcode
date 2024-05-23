3af6d325809614d7fa0d1cb5fd1dda40753a484e
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-05-23 15:28:19 +0000

    comparision operators

---
+++ 01_basics/comparision.js
@@ -0,0 +1,9 @@
+ console.log(null >0);
+ console.log(null==0);
+ console.log(null >=0);
+
+ console.log(undefined ==0);
+ // when used comparitor operator with null, it automatically gets converted to 0 i,e null = 0
+ //so null(0)>0 is false and null(0)>=0 is true
+ //thats why === is used at it is strict and it will not convert it to anything and also checks datatype
+
