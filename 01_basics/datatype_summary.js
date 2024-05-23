25d244e1577a6895eca964b31fafded1e72e7235
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-05-23 15:46:24 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,17 @@
+// primitive(call by value)
+// 7 types: strings,number,boolean,null(means empty),undefined(means value of a variable is not yet defined),symbol(uniqueness),BigInt
+
+
+
+//Reference type (non primitive)
+//arrays,objects,functions
+//return type of arrays and functions is FUNCTION and return type of function is OBJECT FUNCTION
+const heros = ["Spiderman","superman","batman"]
+ let obj = {
+    name:"preksha",
+    age:24
+ }
+
+  let myFunction =function(){
+    console.log("hello world");
+  }
