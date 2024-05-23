89407d44697d49e1aec7ec1042ec31ce1e2adca0
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-05-23 15:17:48 +0000

    operators

--- 01_basics/conversionOperators.js
+++ 01_basics/conversionOperators.js
@@ -21,3 +21,29 @@ let someNum = 33
 let stringNumber = String(someNum)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//////Operations/////
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "preksha"
+let str2 = "N c"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2 +2); // 122
+console.log(1+2+"2");  //32
+
+// tricky conversions
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
