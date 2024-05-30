ff6587c8ee577ecf8732ca535edf12703de2d5e0
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-05-30 14:16:05 +0000

    more of scopes

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -8,19 +8,42 @@ if(true){ // everything which is written inside a block is called BLOCK scope
 // var can be declared anywhere, but let and const can be accessed outside of the scope
 console.log(c)
 
-//nested scope
+/// nested scope
+
 function one(){
-    const username = "hitesh"
- function two(){
-    const website = "youtube"
-    console.log(username);
- }
- //console.log(website)
- two()
+   const firstName = "prekshaN"
+   function two(){
+      const website = "youtube"
+      console.log(firstName);
+   }
+   //console.log(website);//this scope is in function two because line by line execution takes place
+   two()
 }
 one()
 
+if (true) {
+   const firstName = "prekshaa"
+   if (firstName==="prekshaa") {
+      const website = " insta"
+      console.log(firstName+website);
+      
+   }
+  // console.log(website);
+   
+}
+
+//console.log(firstName);
 
 
+//////////////////////////////////////important
+console.log(addone(5))
+function addone(num){ //when function is written as function then it can be called anytime before or after declaring
+   return num+1
+}
+addTwo(5) // throws error that addTwo() cannot be accessed before initialisation
+///can be written like this as well where a function is deaclred and holded in a variable, when done like this it cannot be called before declaring
+const addTwo = function(num){
+   return num+2
+}
 
 
