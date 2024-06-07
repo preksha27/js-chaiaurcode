f5020a06bcc6211211c5d7ef54c3ee3dcf059275
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-06-07 08:38:54 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while
+let i = 0
+while (i<=10) {
+    console.log(`value of index is ${i}`);
+    i = i+2;
+}
+
+const myArrr = ["flash","superman","batman"]
+let arr=0
+while(arr<myArrr.length){
+    console.log(myArrr[arr]);
+    arr++
+}
+
+// do while // work will be done first and condition will be checked later
+let sco = 1
+do {
+    console.log(`Score is ${sco}`);
+    sco++
+} while (sco <=10);
