14ff829ff6f74529825d64e9e4e4f8af92d449fa
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-06-22 05:59:07 +0000

    filter, map and reduce

---
+++ 05_iterations/nine.js
@@ -0,0 +1,29 @@
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce((acc,currval)=>{
+//     console.log(`acc: ${acc} and current value: ${currval}`);
+//     return acc + currval
+// },0)
+const myTotal = myNums.reduce((acc, currval)=> acc+currval,0)
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName :"js course",
+        price:2999
+    },
+    {
+        itemName:"java course",
+        price:999
+    },
+    {
+        itemName:"DSA",
+        price:1999
+    },
+    {
+        itemName:"data science course",
+        price:12999
+    }
+]
+const priceToPay=shoppingCart.reduce((acc,item)=>acc + item.price,0)
+console.log(priceToPay);
