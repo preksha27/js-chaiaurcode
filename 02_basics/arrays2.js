a610c837310134819bced4c33d7eccd238fd0691
preksha27 <55570657+preksha27@users.noreply.github.com>
2024-07-10 08:23:40 +0000

    new notes added

--- 02_basics/arrays2.js
+++ 02_basics/arrays2.js
@@ -11,14 +11,14 @@ const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_anotherArray = anotherArray.flat(Infinity)
 console.log(real_anotherArray)
 
-console.log(Array.isArray("Preksha"))
-console.log(Array.from("preksha"))
+console.log(Array.isArray("Preksha")) // asks if the given item is array or not // output-> false
+console.log(Array.from("preksha"))// converts given string to array // output -> ['p','r','e','k','s','h','a']
 
-console.log(Array.from({name : "preksha"}))//important
+console.log(Array.from({name : "preksha"}))//important // it returns {} because it will not know what to convert in object so we have to specify what to convert
 
 let score1 = 100;
 let score2 = 200;
 let score3 = 300;
 
-console.log(Array.of(score1,score2,score3))
+console.log(Array.of(score1,score2,score3)) // returns new elements from set of array
 
